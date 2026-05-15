1efe134f94466aff101a49b17b45eeeadccf1058
mark rosenberg <mark@does.work>
2026-05-14 21:11:56 -0400

    remove spinner

--- lua/code-buddy/init.lua
+++ lua/code-buddy/init.lua
@@ -5,7 +5,6 @@ function M.setup()
     print(vim.api.nvim_buf_get_name(0))
   end, {})
 
-  vim.api.nvim_create_user_command("CodeBuddySpinner", require("code-buddy.commands.spinner"), {})
   vim.api.nvim_create_user_command("CodeBuddyMeta", require("code-buddy.commands.meta_data"), {})
   vim.api.nvim_create_user_command("CodeBuddyClear", function()
     require("code-buddy.commentor.injector").clear(vim.api.nvim_get_current_buf())
